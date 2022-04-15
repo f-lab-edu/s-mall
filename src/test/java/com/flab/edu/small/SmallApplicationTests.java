921a2d929ecb975dc478be8dec87be25de1873fb
sunggwon <lsghacker@naver.com>
2022-04-15 12:51:18 +0900

    프로젝트 생성

---
+++ src/test/java/com/flab/edu/small/SmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.edu.small;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
