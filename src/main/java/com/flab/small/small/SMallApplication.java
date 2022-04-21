0c28ea4c9e212bd2f8ff5281beded29b7787494e
sunggwon <lsghacker@naver.com>
2022-04-21 17:46:03 +0900

    프로젝트 생성
    신규 프로젝트 생성
    
     jdk : openJDK 1.8
     spring boot : 2.6.6

---
+++ src/main/java/com/flab/small/small/SMallApplication.java
@@ -0,0 +1,13 @@
+package com.flab.small.small;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/small/small/SMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.small.small;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
