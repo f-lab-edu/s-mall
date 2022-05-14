f2b482183c0be14ca303f3196b060f94b5dfc2f7
KJW921222 <wtmqus2@naver.com>
2022-05-14 22:14:09 +0900

    프로젝트 세팅
    ########
    1. 패키지 구조 선정, 패키지 생성
    
    ########
    issue#33

--- src/main/java/com/flabedu/small/small/domain/Item.java
+++ src/main/java/com/flabedu/small/small/domain/Item.java
@@ -1,35 +1,5 @@
 package com.flabedu.small.small.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.validation.constraints.NotEmpty;
-import java.math.BigDecimal;
-import java.util.List;
-
-@Getter
 public class Item {
-
-    private long itemId;
-
-    @NotEmpty(message = "상품을 입력해주세요.")
-    private String name;
-
-    @Setter
-    private String nameEng;
-
-    @NotEmpty(message = "성별을 선택해주세요.")
-    private GenderEnum gender;
-
-    @NotEmpty(message = "상품의 가격을 입력해주세요.")
-    private BigDecimal price;
-
-    private long categoryId;
-    private long subCategoryId;
-    private List<String> itemImages;
-    private List<ItemDetail> itemDetails;
-
-    @Setter
-    private String myUserId;
 
 }
---
+++ src/main/java/com/flabedu/small/small/service/ItemService.java
@@ -0,0 +1,8 @@
+package com.flabedu.small.small.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+}
