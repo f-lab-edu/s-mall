255b06ecf45011e7bff0aa0e263a82dedffb4eb4
KJW921222 <wtmqus2@naver.com>
2022-05-28 17:52:39 +0900

    ## 작업사항 1. 불필요한 공백 제거 2. 테스트 코드 @DisplayName 수정 3. ItemDTO의 itemId 제거 4. dto - itemDetailDTO 클래스 추가    * itemDetail로 변환하는 메소드 작성 5. model - ItemImage , ItemCategory 클래스 추가 6. ErrorCode Enum 클래스 추가    * CutsomException 필드 수정    * GlobalExceptionHandler 수정 7. model - enums 패키지 생성    * GenderEnum, SizeEnum 이동 8. ItemControllerTest 수정    * 부족한 파라미터 개수로 인한 상품 등록 실패 테스트 케이스 작성
     ## 기타사항
     패키지 구성 방법을 계층형 구조로 선정했기 때문에, enum을 사용하는 계층에 enum 클래스를 넣어두는 것이 가장 적합하다고 생각하여 model 하위에 enum 클래스를 위치시키도록 함. 계층형 구조이므로 model에 너무 많은 클래스가 작성될 수 있어, model - enums 패키지를 생성하여 그 하위에 enum 클래스를 위치시킴.

--- src/main/java/com/flabedu/small/small/exception/ErrorCode.java
+++ src/main/java/com/flabedu/small/small/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
     NOT_FOUND_ID(400, "가입되지 않은 ID입니다."),
     MISMATCH_PASSWORD(400, "잘못된 비밀번호입니다."),
     INVALID_PASSWORD(400, "잘못된 비밀번호 양식입니다."),
-    INVALID_ARGUMENT(400, "요청 값이 유효하지 않습니다."),
+    INVALID_ARGUMENT(400, "요청 파라미터가 유효하지 않습니다."),
     NOT_ENOUGH_STOCK(400, "해당 상품의 재고가 부족합니다.");
 
     private int status;
